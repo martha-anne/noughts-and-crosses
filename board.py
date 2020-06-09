71687a664ab92593b829c25f93d8cad2bfcf116e
Martha Anne <miss.martha.anne@googlemail.com>
2020-06-09 12:09:37 +0100

    boardstatedecider has logic to detect input which is too long or containing invalid characters

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+import re
+
 class BoardState:
     NOUGHTS_WIN = 'NOUGHTS_WIN'
     CROSSES_WIN = 'CROSSES_WIN'
@@ -6,11 +8,34 @@ class BoardState:
     INVALID = 'INVALID' # This will be used in the case that the numer of Xs is not equal to or one greater than the number of Os.
     # Do we also need to return an INVALID state if we recieve a board where both Xs and Os have formed a line (eg XXXOOO___)? 
 
+class BoardInput:
+    CROSS = "X"
+    NAUGHT = 'O'
+    EMPTY = "_"
+
 class BoardStateDecider:
 
+    
+
     def __init__(self, board_definition: str):
-        # Assign the argument to the instance's name attribute
-        self.board_definition = board_definition
+        self._board_definition_length = 9
+        self._re_is_board_definition_supported_length_and_characters = "^[XO_]{" + str(self._board_definition_length) + "}$"
+        self._re_is_board_full = "^[XO]{" + str(self._board_definition_length) + "}$"
+
+        self.board_definition = board_definition.upper()
 
     def decide_state_of_board(self) -> BoardState:
-        return BoardState.INVALID
+
+        if not self.__is_board_definition_supported_length_and_characters():
+            return BoardState.INVALID
+        
+        raise Exception("Error")
+
+
+    def __is_board_definition_supported_length_and_characters(self) -> bool:
+        pattern = re.compile(self._re_is_board_definition_supported_length_and_characters)
+        return pattern.fullmatch(self.board_definition) is not None
+
+    def __is_board_full(self) -> bool:
+        pattern = re.compile(self._re_is_board_full)
+        return pattern.fullmatch(self.board_definition) is not None
